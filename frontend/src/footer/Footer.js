3332a583d8fb1b39db20c915cc9e3ce5cdd06674
Martin Kinaro <njengamartin72@gmail.com>
2021-10-18 06:55:43 +0300

    First Commit

---
+++ frontend/src/footer/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "../Home.css";
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <footer>
+          <p>Pata Tutor, Copyright &copy; 2021</p>
+        </footer>
+      </div>
+    );
+  }
+}
